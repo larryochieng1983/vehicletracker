f5ef2791033d54f7c7b00278bc0a248987f821ff
otieno <otieno@tracetracker.com>
2013-11-27 09:34:40 +0300

    GPS Servlet

--- src/main/java/com/wizglobal/vehicletracker/beans/VehiclePositionBean.java
+++ src/main/java/com/wizglobal/vehicletracker/beans/VehiclePositionBean.java
@@ -4,9 +4,14 @@
 package com.wizglobal.vehicletracker.beans;
 
 import java.io.Serializable;
+import java.util.logging.Logger;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+
+import com.wizglobal.vehicletracker.service.GpsDeviceService;
+import com.wizglobal.vehicletracker.service.VehiclePositionService;
 
 /**
  * @author Otieno Lawrence
@@ -20,6 +25,11 @@ public class VehiclePositionBean implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private transient Logger logger;
+
+	@Inject
+	private VehiclePositionService vehiclePositionService;
 
 	public VehiclePositionBean() {
 
---
+++ src/main/java/com/wizglobal/vehicletracker/beans/VehiclePositionServlet.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.wizglobal.vehicletracker.beans;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Receives vehicle position updates from GPS Devices
+ * 
+ * @author Otieno Lawrence
+ * 
+ */
+public class VehiclePositionServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
+	 */
+	@Override
+	public void init( ServletConfig servletConfig ) throws ServletException {
+		super.init( servletConfig );
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest,
+	 * javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doGet( HttpServletRequest request, HttpServletResponse response )
+			throws ServletException, IOException {
+		super.doGet( request, response );
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest,
+	 * javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doPost( HttpServletRequest request, HttpServletResponse response )
+			throws ServletException, IOException {
+		super.doPost( request, response );
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.GenericServlet#destroy()
+	 */
+	@Override
+	public void destroy() {
+		super.destroy();
+	}
+
+}
