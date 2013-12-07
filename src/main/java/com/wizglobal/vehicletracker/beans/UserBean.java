e0f8d01cb7f7d7195e7a56060a7fdfb42f624d38
Kenny Kosgei <kosgei.kenny@gmail.com>
2013-12-07 23:16:26 +0300

    Add resolved conflicts

--- src/main/java/com/wizglobal/vehicletracker/beans/UserBean.java
+++ src/main/java/com/wizglobal/vehicletracker/beans/UserBean.java
@@ -5,7 +5,6 @@ package com.wizglobal.vehicletracker.beans;
 
 import com.wizglobal.vehicletracker.domain.User;
 import com.wizglobal.vehicletracker.domain.UserRole;
-import com.wizglobal.vehicletracker.exception.DataAccessException;
 import com.wizglobal.vehicletracker.service.UserService;
 import java.io.Serializable;
 import java.util.List;
@@ -66,7 +65,6 @@ public class UserBean implements Serializable {
      * Create, Update and Delete operations
      */
     public void doCreateUser() {
-	try {
 	    das.create(newUser);
 	} catch (Exception ex) {
 	    Logger.getLogger(UserBean.class.getName()).log(Level.SEVERE, null, ex);
--- src/main/java/com/wizglobal/vehicletracker/util/JpaUtil.java
+++ src/main/java/com/wizglobal/vehicletracker/util/JpaUtil.java
@@ -3,14 +3,12 @@
  */
 package com.wizglobal.vehicletracker.util;
 
+import com.wizglobal.vehicletracker.domain.BaseEntity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.wizglobal.vehicletracker.domain.BaseEntity;
 
 /**
  * @author Otieno Lawrence
