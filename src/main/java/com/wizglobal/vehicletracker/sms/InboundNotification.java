8bea317ca482ff866c6b04d2fbb0d4a4889b6d83
otieno <otieno@tracetracker.com>
2014-02-10 22:18:58 +0300

    create vehicle postion

--- src/main/java/com/wizglobal/vehicletracker/sms/InboundNotification.java
+++ src/main/java/com/wizglobal/vehicletracker/sms/InboundNotification.java
@@ -4,6 +4,7 @@
 package com.wizglobal.vehicletracker.sms;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -16,7 +17,12 @@ import org.smslib.InboundMessage;
 import org.smslib.Message.MessageTypes;
 
 import com.wizglobal.vehicletracker.domain.IncomingSms;
+import com.wizglobal.vehicletracker.domain.Vehicle;
+import com.wizglobal.vehicletracker.domain.VehiclePosition;
+import com.wizglobal.vehicletracker.map.GmapUrlHelper;
 import com.wizglobal.vehicletracker.service.IncomingSmsService;
+import com.wizglobal.vehicletracker.service.VehiclePositionService;
+import com.wizglobal.vehicletracker.service.VehicleService;
 
 /**
  * @author Otieno Lawrence
@@ -35,6 +41,11 @@ public class InboundNotification implements IInboundMessageNotification, Seriali
 
 	@Inject
 	private IncomingSmsService incomingSmsService;
+	@Inject
+	private VehicleService vehicleService;
+	@Inject
+	private VehiclePositionService vehiclePositionService;
+	
 
 	public void process( AGateway gateway, MessageTypes msgType, InboundMessage msg ) {
 		if( msgType == MessageTypes.INBOUND ) {
@@ -45,9 +56,19 @@ public class InboundNotification implements IInboundMessageNotification, Seriali
 			incomingSms.setMessageDate( msg.getDate() );
 			incomingSms.setReceiveDate( msg.getDate() );
 			incomingSms.setMessage( msg.getText() );
-			if( incomingSmsService != null ) {
-				incomingSmsService.create( incomingSms );
+			incomingSmsService.create( incomingSms );
+			
+			//create the vehicle position
+			VehiclePosition position = new VehiclePosition();
+			Vehicle vehicle = null;
+			List<Vehicle> list = vehicleService.findWithNamedQuery( "findVehicleByPhoneNumber",
+					parameters );
+			if( !list.isEmpty() ) {
+				vehicle = list.get( 0 );
 			}
+			position.setVehicle( vehicle );
+			position.setTimeTracked( incomingSms.getMessageDate() );
+			position.setGpsPosition( GmapUrlHelper.getGpsPostion( incomingSms.getMessage() ) );
 		} else if( msgType == MessageTypes.STATUSREPORT ) {
 			log.info( ">>> New Inbound Status Report message detected from Gateway: "
 					+ gateway.getGatewayId() );
